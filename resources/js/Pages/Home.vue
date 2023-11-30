167dd777d66f35d69e28a8759272b84a80e0f69c
Nerenso <Nerenso@hotmail.com>
2023-11-30 16:55:40 +0100

    Setup Indielayer component library

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <Head title="Home" />
+  <main class="min-h-screen w-full flex flex-col bg-slate-100">
+    <nav class="w-full">
+      <div class="mx-auto max-w-5xl w-full bg-green-300 p-4 flex items-center justify-between">
+        <h1 class="text-4xl font-bold">
+          <a href="#">Logo</a>
+        </h1>
+        <div class="flex items-center justify-between gap-4">
+          <Link :href="route('login')">Login</Link>
+          <Link :href="route('register')">Register</Link>
+          <XButton>Button</XButton>
+        </div>
+      </div>
+    </nav>
+
+    <section class="flex-1 w-full">
+      <div class="mx-auto max-w-5xl w-full">CONTENT</div>
+    </section>
+  </main>
+</template>
+
+<script setup>
+import { XButton } from "@indielayer/ui";
+import { Head, Link } from "@inertiajs/vue3";
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,32 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+const colors = require("tailwindcss/colors");
+const indielayer = require("@indielayer/ui/tailwind.preset");
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.vue",
+    "node_modules/@indielayer/ui/{lib,src}/**/*",
+  ],
+  presets: [indielayer()],
+  theme: {
+    extend: {
+      fontFamily: {
+        // sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: colors.emerald,
+        secondary: colors.slate,
+        success: colors.green,
+        warning: colors.yellow,
+        error: colors.red,
+      },
     },
+  },
 
-    plugins: [forms],
+  plugins: [forms],
 };
