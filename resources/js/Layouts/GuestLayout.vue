123124bb392ccb32760e6beba5bbb9bb456056f3
Nerenso <Nerenso@hotmail.com>
2024-04-05 16:19:33 +0200

    Add correct favicon files

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -6,17 +6,22 @@
       </Link>
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-      <slot />
-      <div v-if="errorMessage" class="fixed bottom-8 right-8 p-4 bg-white rounded-lg shadow-xl border border-l-4 border-red-500">
-        <p>{{ page.props.flash.message }}</p>
-      </div>
+    <div class="w-full p-2">
+      <!-- <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"> -->
+      <ContentBox class="p-6 max-w-md w-full mt-6 mx-auto">
+        <slot />
+        <div v-if="errorMessage" class="fixed bottom-8 right-8 p-4 bg-white rounded-lg shadow-xl border border-l-4 border-red-500">
+          <p>{{ page.props.flash.message }}</p>
+        </div>
+      </ContentBox>
     </div>
+    <!-- </div> -->
   </div>
 </template>
 
 <script setup>
 import ApplicationLogo from "@/Components/Breeze/ApplicationLogo.vue";
+import ContentBox from "@/Components/UI/ContentBox.vue";
 import { Link, usePage } from "@inertiajs/vue3";
 import { computed, ref, watch } from "vue";
 
