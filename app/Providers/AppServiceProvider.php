9f9491c534c64c0e8b33f65912d3d734fc2d7559
Nerenso <Nerenso@hotmail.com>
2025-03-13 20:38:45 +0100

    Add telescope package for debugging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,24 +8,27 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    if ($this->app->environment('local')) {
+      $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+      $this->app->register(TelescopeServiceProvider::class);
     }
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        // \Debugbar::disable();
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(): void
+  {
+    // \Debugbar::disable();
 
-        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
-            return (new MailMessage)
-                ->view('emails.transactional', ['url' => $url, 'user' => $notifiable, 'name' => 'John']);
-        });
-    }
+    VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+      return (new MailMessage)
+        ->view('emails.transactional', ['url' => $url, 'user' => $notifiable, 'name' => 'John']);
+    });
+  }
 }
